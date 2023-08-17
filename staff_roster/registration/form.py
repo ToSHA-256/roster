b4fa4a0fae65d4f7e948510935eb0642e9ff0396
toSHA-256 <antonsmorgun@gmail.com>
2023-08-17 18:00:31 +0300

    fix

--- staff_roster/registration/form.py
+++ staff_roster/registration/form.py
@@ -3,7 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 
-class CustomUserCreationForm(UserCreationForm):
+class RegisterUserForm(UserCreationForm):
+    first_name = forms.CharField(label='Имя:', widget=forms.TextInput(attrs={'class': 'blank-input'}))
+    patronymic = forms.CharField(label='Отчество:', widget=forms.TextInput(attrs={'class': 'blank-input'}))
+    last_name = forms.CharField(label='Фамилия:', widget=forms.TextInput(attrs={'class': 'blank-input'}))
+    password1 = forms.CharField(label='Пароль:', widget=forms.PasswordInput(attrs={'class': 'blank-input'}))
+    password2 = forms.CharField(label='Подтверждение пароля:',
+                                widget=forms.PasswordInput(attrs={'class': 'blank-input'}))
+
     class Meta:
         model = CustomUser
-        fields = ('username', 'first_name', 'last_name', 'patronymic', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'patronymic', 'password1', 'password2')
