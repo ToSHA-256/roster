34c550b7c6458106715d75bc2cd1d04aeb73bed2
toSHA-256 <antonsmorgun@gmail.com>
2023-08-18 12:24:01 +0300

    filter

--- staff_roster/custom_admin/views.py
+++ staff_roster/custom_admin/views.py
@@ -2,19 +2,20 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from base.models import Department, Sector
 from .forms import EmployeeForm
+from base.models import Management
 
 
 def employee_form(request):
     form = EmployeeForm()
-    return render(request, 'custom_admin/employee_form.html', {'form': form})
+    management_choices = Management.objects.all()  # Получение всех управлений
+    return render(request, 'custom_admin/employee_form.html', {'form': form, 'management_choices': management_choices})
+
 
 
 def get_departments(request):
     management_id = request.GET.get('management_id')
     departments = Department.objects.filter(management_id=management_id)
     data = [{'id': dep.id, 'name': dep.name} for dep in departments]
-    print(management_id)
-    print(departments)
     return JsonResponse(data, safe=False)
 
 
